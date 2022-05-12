19fca266224aff5d52b9fdf1bf0c93283ed3464c
junlistar <450152445@qq.com>
2022-05-12 08:53:32 +0800

    connection string

--- CoreData/DataBaseConfig.cs
+++ CoreData/DataBaseConfig.cs
@@ -10,8 +10,8 @@ namespace Data
         #region SqlServer链接配置
 
         
-        private static string DefaultSqlConnectionString = @"Data Source = 10.102.41.147; Initial Catalog = his; User ID = sa; Password=gx@2019;";
-        //private static string DefaultSqlConnectionString = Helpers.ConfigHelper.GetConnectionString("ConnectionString");
+        //private static string DefaultSqlConnectionString = @"Data Source = 10.102.41.147; Initial Catalog = his; User ID = sa; Password=gx@2019;";
+        private static string DefaultSqlConnectionString = Helpers.ConfigHelper.GetConnectionString("ConnectionString");
         //private static string DefaultSqlConnectionString = ConfigurationManager.AppSettings.Get("ConnString");
         private static string DefaultRedisString = "localhost, abortConnect=false";
         private static ConnectionMultiplexer redis;
